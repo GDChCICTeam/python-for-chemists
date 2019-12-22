e7dba5d698f8759ae841a1525aa15cabdfb405b2
Emanuel Ehmki <emanuel.ehmki@gmail.com>
2019-12-22 23:24:21 +0100

    Testing/Travis: add travis config and testing script
        - add travis requirements and config yaml files
        - add python unittest file to test the files in the solutions
          folder

---
+++ Solutions/test_solutions.py
@@ -0,0 +1,14 @@
+def test_loops():
+    __import__('Loops')
+
+
+def test_functions():
+    __import__('Functions')
+
+
+def test_conditionals():
+    __import__('Conditionals')
+
+
+def test_variables():
+    __import__('Variables')
